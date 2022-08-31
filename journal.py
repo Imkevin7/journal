f55e258982987f428dff31cae66c6f61cbbe6fd6
kevinpichette <kevin.pichette@gmail.com>
2022-08-31 10:08:52 -0700

    idk

--- journal.py
+++ journal.py
@@ -1,29 +1,37 @@
 def main():
-    
+    import os
+    file_stat = os.stat('journal.txt')
+    print(f"File Size: {file_stat.st_size}")
     import datetime
     x = datetime.datetime.now()
-    x = x.strftime("\n\n%B, %A-%d, %G - %I:%M %p\n")
-    x = str(x)
+    x = str(x.strftime("\n\n%A, %B %d, %G - %I:%M %p\n"))
     the_file(x)
 
 def read_file():
     f = open("journal.txt", "r")
     print(f.read())
+    f.close()
+    print()
 
 
 def the_file(x):
-    # view last entry
-    read_file()
+    while True:
+        try:
+            # view last entry
+            read_file()
 
-    # Ask for new entry today
-    myText = str(input("What should I say today? "))
+            # Ask for new entry today
+            myText = str(input("What should I say today? ").strip())
 
-    # write the new journal entry to file
-    f = open("journal.txt", "a")
-    f.write(x + myText)
-    f.close()
+            # write the new journal entry to file
+            f = open("journal.txt", "a")
+            f.write(x + myText)
+            f.close()
 
-    # read entry just entered
-    read_file()
+            # read entry just entered
+            read_file()
+        
+        except EOFError:
+           break
 
 main()
